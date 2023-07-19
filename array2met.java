428fb6a82f65ebd9aa85d3d43eb6ce06960983ed
Rishi Ragav <63560344+rishi-ragav@users.noreply.github.com>
2023-07-19 15:55:55 +0530

    Add files via upload

---
+++ array2met.java
@@ -0,0 +1,16 @@
+/**
+ * array2met
+ */
+public class array2met {
+
+    static int[] get()
+    {
+        return new int[]{10,20,30};
+    }
+    public static void main(String[] args) {
+        int a[]=get();
+        for (int i : a) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ impleforeac.java
@@ -0,0 +1,8 @@
+public class impleforeac {
+    public static void main(String[] args) {
+        int a[] = {33,3,4,5};
+        for (int i : a) {
+            System.out.println(i);
+        }
+    }
+}
