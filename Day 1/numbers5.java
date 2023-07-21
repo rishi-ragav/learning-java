9cbe0770161dc0cacaf6e1ae98f2e6b404ad0921
Rishi Ragav <rishiragav1@gmail.com>
2023-07-21 13:36:21 +0530

    added files to day 1

---
+++ Day 1/numbers5.java
@@ -0,0 +1,9 @@
+/*Write a program to print 5 numbers */
+
+public class numbers5 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(i+" ");
+        }
+    }
+}
---
+++ Day 1/sumof5.java
@@ -0,0 +1,23 @@
+
+//program to find the sum of 5 positive numbers entered by the user.
+import java.util.Scanner;
+
+public class sumof5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        
+        System.out.println("Enter 5 positive numbers:");
+        for (int i = 0; i < 5; i++) {
+            int number = scanner.nextInt();
+            if (number > 0) {
+                sum += number;
+            } else {
+                System.out.println("Invalid input! Please enter a positive number.");
+                i--; // Decrement i to repeat the current iteration
+            }
+        }
+        scanner.close();
+        System.out.println("The sum of the positive numbers is: " + sum);
+    }
+}
