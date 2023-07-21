744414522a6ee6705ed6eb3ccb432fffecefb432
Rishi Ragav <rishiragav1@gmail.com>
2023-07-21 14:37:46 +0530

    added Day 5

---
+++ Day 5/student.java
@@ -0,0 +1,20 @@
+
+
+
+    public class student 
+{
+    int rollno;
+    String name;
+    double sub1,sub2,sub3;
+    double total;    
+    
+
+
+public void average()
+{
+     double m = total/3;
+    System.out.println("Average: " + m);
+
+}
+}
+
