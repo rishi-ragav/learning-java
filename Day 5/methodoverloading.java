3c63fdd74845ee0672cea50beb8ec5342754c74d
Rishi Ragav <rishiragav1@gmail.com>
2023-07-21 15:35:31 +0530

    done

--- Day 5/methodoverloading.java
+++ Day 5/methodoverloading.java
@@ -12,6 +12,6 @@ class Adder{
 class methodoverloading{
     public static void main(String[] args) {
         System.out.println(Adder.add(11, 11));
-        System.out.println(Adder.add(11, 11, 11));
+        System.out.println(Adder.add(11, 12, 13));
     }
 }
